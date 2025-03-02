c70eebe7ec85994dfc1334383749537a3917ac1d
n1mply <140534843+n1mply@users.noreply.github.com>
2025-03-02 16:16:54 +0300

    Update currencies.py

--- Backend/currencies.py
+++ Backend/currencies.py
@@ -1,6 +1,6 @@
 import freecurrencyapi
 
-client = freecurrencyapi.Client('fca_live_7k0b0ox5cPrBT6BhX1Jk25peBMRMp1WypfSUwmND')
+client = freecurrencyapi.Client('freecurrencyapikey')
 
 def get_currencies(currencies: list):
     result = client.currencies(currencies=currencies)
